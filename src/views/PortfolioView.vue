cb99abc72f1056d1b69ccdb42880ada5a7ac89ca
Alejandro Tapia <tapia6176@gmail.com>
2024-09-08 18:30:35 -0700

    notable

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -62,7 +62,7 @@ export default {
         <div
           class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
           <!-- <div class="h-[1px] w-10 bg-amber-200 md:w-20 aos-init aos-animate"></div> -->
-          <h4>Past Project Experience</h4>
+          <h4>Notable Project Experience</h4>
           <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-amber-300">
             Explore the projects I've worked on so far</h4>
         </div>
